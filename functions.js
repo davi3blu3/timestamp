106b6a425225d43f1fd43e96fc137416022de4dc
Dave Harned <dharned@gmail.com>
2017-03-10 18:32:40 +0000

    refactor to convert unix string to num

--- functions.js
+++ functions.js
@@ -1,20 +1,57 @@
 module.exports = {
+    
+    formatNumStr: function(date) {
+      if (typeof date === "string") {
+        for (var i = 0; i < date.length; i++) {
+          if (isNaN( parseInt(date[i]))) {
+            return date;
+          }
+        }
+      }
+      return parseInt(date);
+    },
+    
     processParam: function(date, fn) {
-
-        var input = new Date(date);
+        var d         = this.formatNumStr(date),
+            input     = new Date(d),
+            unix      = input.getTime(),
+            month     = input.toLocaleString("en-us", { month: "long" }),
+            day       = input.getDate(),
+            year      = input.getFullYear(),
+            formatted = month + " " + day + ", " + year,
+            result    = {};
         
-        if (input.toDateString() != "Invalid Date") {
-            var result = {
-                "unix":     input.getTime(),
-                "natural":  input.toDateString()
+        if (month === "Invalid Date") {
+            result = {
+                "unix":     null,
+                "natural":  null                
             };
         } else {
-            var result = {
-                "unix":     null,
-                "natural":  null
+            result = {
+                "unix":     unix,
+                "natural":  formatted                
             };
         }
         return fn(result);
     } 
 };
 
+
+
+
+
+
+function runDate (date) {
+  var d = formatNumStr(date);
+  d = new Date(d);
+
+  var objDate   = new Date(d),
+    locale    = "en-us",
+    month     = objDate.toLocaleString(locale, { month: "long" }),
+    day       = objDate.getDate(),
+    year      = objDate.getFullYear(),
+    formatted = month + " " + day + ", " + year;
+
+  if (month === "Invalid Date") console.log("null, fool");
+  else console.log(formatted);
+}
