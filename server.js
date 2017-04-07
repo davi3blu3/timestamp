c77e84c6fe7d1ac02fe49ff5aa2aa86c90673852
Dave Harned <dharned@gmail.com>
2017-04-07 00:56:23 +0000

    add minimal styling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var func = require('./functions.js');
 
+app.use(express.static('public'))
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 })
